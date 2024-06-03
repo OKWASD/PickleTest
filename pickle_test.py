46a779fd869ce6a577c9cc066e311488cf5028d0
Hadi Abou Zahra <guntrix3rhd@gmail.com>
2024-06-03 15:39:40 +0200

    Added third test

--- pickle_test.py
+++ pickle_test.py
@@ -1,6 +1,7 @@
 import pickle
 import math
 import unittest
+import hashlib
 
 class TestPickle(unittest.TestCase):
     def test_associative(self):
@@ -18,7 +19,26 @@ class TestPickle(unittest.TestCase):
         g = pickle.dumps(nested_list)
         g_load = pickle.loads(g)
         
-        self.assertEqual(nested_list, g_load)      
+        self.assertEqual(nested_list, g_load)
+        
+    def test_hashing_equal(self):
+        #Vi hashar 2st och self.assertEqual()
+        
+        db = pickle.dumps(first_to_second(1,2))
+        db1 = pickle.dumps(second_to_first(1,2))
+        
+        hashed_db = hashlib.sha256(db)
+        hashed_db1 = hashlib.sha256(db1)
+                
+        self.assertEqual(hashed_db.hexdigest(), hashed_db1.hexdigest())
+
+
+
+def first_to_second(a,b):
+    return a + b
+
+def second_to_first(a,b):
+    return b + a
         
 if __name__ == '__main__':
     unittest.main()
